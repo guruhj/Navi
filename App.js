63b6e5a77d956ae3b6f3d207cf2485d0620ceb3d
guruhj <guruhj@gmail.com>
2019-04-03 16:46:27 +0900

    0403 commit

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 
+// 현재는 이동되는 화면이 모두 한 파일에 들어있음. 
+// 향후에는 각각의 화면을 별도 파일로 분리하고, stackNavigator도 별도 파일로 분리하는 방법 확인 필요함
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 class HomeScreen extends React.Component {
+
+  static navigationOptions = {    // static 객체를 선언
+    title: 'Home',
+  };
+
+  // 각 화면에 설정된 navigationOptions 가 우선순위가 높음
+ /*  static navigationOptions = {    // static 객체를 선언
+    title: 'Home',
+    headerStyle : {
+      backgroundColor : '#7C7CFF',
+    },
+    headerTintColor : '#fff',
+    headerTitleStyle : {
+      fontWeight : 'bold',
+    },
+  };
+ */
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -25,6 +44,25 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
+
+  /* static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('otherParam', 'A nested Details Screen'),
+    };
+  }; */
+
+  static navigationOptions = ({navigation, navigationOptions}) => {
+    console.log(navigationOptions);
+
+    return{
+      title : navigation.getParam('otherParam','A Nested Details Screen'),
+      headerStyle : {
+        backgroundColor : navigationOptions.headerTintColor,
+      },
+      headerTintColor : navigationOptions.headerStyle.backgroundColor,
+    }
+  }
+
   render() {
     /* 2. Get the param, provide a fallback value if not available */
     const { navigation } = this.props;
@@ -44,6 +82,11 @@ class DetailsScreen extends React.Component {
             })}
         />
         <Button
+          title="Update the title"
+          onPress={() =>
+            this.props.navigation.setParams({ otherParam: "Updated!" })}
+        />
+        <Button
           title="Go to Home"
           onPress={() => this.props.navigation.navigate('Home')}
         />
@@ -56,6 +99,7 @@ class DetailsScreen extends React.Component {
   }
 }
 
+// RootStack 하위에 있는 화면은 모두 defaultNavigationOptions의 영향을 받음. 
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
@@ -63,6 +107,16 @@ const RootStack = createStackNavigator(
   },
   {
     initialRouteName: 'Home',
+    /* The header config from HomeScreen is now here */
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f2223a',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
   }
 );
 
